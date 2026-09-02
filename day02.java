e01a0f2319e0b56240cb371c0b3726f39cfd0ac0
alalalalalalalalalakaks8-sketch <alalalalalalalalalakaks8@gmail.com>
2026-09-03 07:50:12 +0800

    Create day02.java

---
+++ day02.java
@@ -0,0 +1,16 @@
+public class Main {
+
+  public static void main(String[] args) {
+    // coding dengan println() 
+    System.out.println("Nama  muh rizky darwis");
+    System.out.println("Alumni smkn labuang");
+
+    //coding dengan print()
+    System.out.print("prody     ");
+    System.out.print("informatika");
+
+    //coding dengan printf()
+    System.out.printf("tempat tinggal   ");
+    System.out.printf("campalagian");
+      }
+}
