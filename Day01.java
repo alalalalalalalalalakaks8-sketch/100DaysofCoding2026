864f259b8eda4807f416ea323c7ad0530a92913f
alalalalalalalalalakaks8-sketch <alalalalalalalalalakaks8@gmail.com>
2026-09-02 22:13:09 +0800

    Day01.java

---
+++ Day01.java
@@ -0,0 +1,11 @@
+public class Main {
+  //day1 belajar ngoding
+
+ /* ini ilmu yang pertama saya dapat
+  * saya mencoba masuk isc agar dapat ilmu baru sama dapat pengalaman baru
+  */
+
+  public static void main(String[] args) {
+    System.out.print("muh rizky darwis");
+      }
+}
