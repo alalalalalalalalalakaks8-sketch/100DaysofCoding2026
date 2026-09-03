8dd73399b140368a6e76c2b7caaa0c79320c3573
alalalalalalalalalakaks8-sketch <alalalalalalalalalakaks8@gmail.com>
2026-09-03 09:32:45 +0800

    Create Day03.java

---
+++ Day03.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Nama  \nmuhrizky\numur:19");
+    System.out.println("Kelas \tinformatika");
+    System.out.println("Folder C:\\java");
+    System.out.println("Pesan \"pejuang ngoding\"");
+  }
+    
+}
