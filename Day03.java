2a0a9710376e698ef9c5ff858113cf1cd82306ee
alalalalalalalalalakaks8-sketch <alalalalalalalalalakaks8@gmail.com>
2026-09-04 08:10:34 +0800

    Update Day03.java

--- Day03.java
+++ Day03.java
@@ -1,8 +1,8 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Nama  \nmuhrizky\numur:19");
-    System.out.println("Kelas \tinformatika");
-    System.out.println("Folder C:\\java");
+    System.out.println("Kelas \tB");
+    System.out.println("Prodi:\\informatika");
     System.out.println("Pesan \"pejuang ngoding\"");
   }
     
