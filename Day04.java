e5c822de7c3f6f8615796fc80cc8001c699cc3b0
alalalalalalalalalakaks8-sketch <alalalalalalalalalakaks8@gmail.com>
2026-09-05 07:22:49 +0800

    Update Day04.java

--- Day04.java
+++ Day04.java
@@ -1,1 +1,20 @@
-
+public class {
+    public static void main(String[] args) {
+        
+        //BYTE -> -128 sampai 127
+        byte umur = 19;
+        System.out.printf("umur : %d tahun\n", umur);
+        
+        //SHORT -> -32.768 sampai 32.767  
+        short jumlahSiswaUnsulbar = 14000;
+        System.out.printf("jumlah\tSiswa\tUnsulbar : %d\n", jumlahSiswaUnsulbar);
+        
+        //INT -> paling umum dipakai
+        int Uangsaku = 15000000;
+        System.out.printf("Uangsaku :Rp %d\n", Uangsaku);
+        
+        //LONG -> harus ada huruf L di belakang
+        long populasisulawesibarat = 1547400L;
+        System.out.printf("Populasi\tsulawesibarat :%d\n",populasisulawesibarat);
+    }
+}
