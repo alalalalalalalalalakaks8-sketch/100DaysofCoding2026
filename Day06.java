05057a047d0029328997232758a43232befb127b
alalalalalalalalalakaks8-sketch <alalalalalalalalalakaks8@gmail.com>
2026-09-06 16:35:04 +0800

    Create Day06.java

---
+++ Day06.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+        
+        // CHAR = Penggunaan huruf 1 saja,pakai ' '
+        char Test1 = '1';
+        char Test2 = '2';
+        
+        // BOOLEAN = true atau false
+        boolean BBMnaik = true;
+        boolean BBMturun = false;
+        
+        // CETAK
+        System.out.println(Test1);
+        System.out.println(Test2);
+        System.out.println(BBMnaik);
+        System.out.println(BBMturun);
+    }
+}
