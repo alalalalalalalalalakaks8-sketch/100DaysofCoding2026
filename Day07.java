1e68468486b05692b58582341c0e891a5c352536
alalalalalalalalalakaks8-sketch <alalalalalalalalalakaks8@gmail.com>
2026-09-08 20:00:54 +0800

    Create Day07.java

---
+++ Day07.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args){
+        
+            // String dapat mengelolah data berupa text
+        String nama = "Muh rizky darwis";
+          String Alamat = "Campalagian";
+        
+            // String dapat menggunakan angka dan simbol
+        String RankEmel = "Glory_50an";
+          String Angkatan = "2026";
+        
+            // String juga dapat dipakaikan tanda +
+        String Univ = "universitas sulawesi barat";
+          String pe = "hai saya anak  " + Univ;
+
+        System.out.println("My name is  "+ nama);
+         System.out.println("I'm from  "+ Alamat);
+           System.out.println("My rank\t"+ RankEmel);
+          System.out.println("Saya angkatan tahun "+ Angkatan);
+        System.out.println(pe);
+    }
+}
