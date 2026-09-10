87f1bede343a7050c503585d3acd11bfbd15ab41
alalalalalalalalalakaks8-sketch <alalalalalalalalalakaks8@gmail.com>
2026-09-10 11:15:58 +0800

    Create Day09.java

---
+++ Day09.java
@@ -0,0 +1,16 @@
+public class App {
+public static void main(String[] args) {
+  int tes = 1;
+String testing = "testes";
+
+System.out.println("tes tes"+ tes);
+System.out.println("tes wee"+ testing);
+final int tamat = 10;
+final String habis = "kosong";
+
+System.out.println("tamat sudah"+ tamat);
+System.out.println("Nda tau"+ habis);
+
+String mengetes = "mengetes saja";
+  }
+}
