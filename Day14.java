7563cb698f7068d1fd01f923dd0c55b0da398635
alalalalalalalalalakaks8-sketch <alalalalalalalalalakaks8@gmail.com>
2026-09-15 16:43:39 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+      int a = 110;
+      int b = 1;
+        int A = a + b;
+      int c = 112;
+      int d = 1;
+        int B = c - d;
+
+System.out.println("Hasil Penjumlahan: " + A);
+System.out.println("Hasil Pengurangan: " + B);
+}
+}
