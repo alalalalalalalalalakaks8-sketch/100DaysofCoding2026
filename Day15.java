9a3fa986c5b19cc33f59bb14f6bfc2788f241aa1
alalalalalalalalalakaks8-sketch <alalalalalalalalalakaks8@gmail.com>
2026-09-15 16:44:27 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+      int a = 110;
+      int b = 2;
+        int A = a * b;
+      int c = 112;
+      int d = 2;
+        int B = c / d;
+
+System.out.println("Hasil Penjumlahan: " + A);
+System.out.println("Hasil Pengurangan: " + B);
+}
+}
