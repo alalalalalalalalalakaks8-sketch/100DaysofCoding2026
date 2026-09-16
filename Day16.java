0b76e169d58db3bcc1982c66b8be6dcf03be3e40
alalalalalalalalalakaks8-sketch <alalalalalalalalalakaks8@gmail.com>
2026-09-16 12:08:58 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,14 @@
+public class App {
+public static void main(String[] args) {
+  
+
+int angka1 = 100;
+int angka2 = 30;
+
+int A = angka1 / angka2;
+int B = angka1 % angka2;
+
+System.out.println("Hasil : "+ A);
+System.out.println("Hasil : "+ B);
+  }
+}
