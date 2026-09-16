0ad8bb2e602a5c116c016b8f0b1ac6249c903c3c
alalalalalalalalalakaks8-sketch <alalalalalalalalalakaks8@gmail.com>
2026-09-16 22:17:12 +0800

    Update Day16.java

--- Day16.java
+++ Day16.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 public class App {
 public static void main(String[] args) {
-  
+  Scanner in = new Scanner (System.in);
 
-int angka1 = 100;
-int angka2 = 30;
+int angka1 = in.nextInt();
+int angka2 = in.nextInt();
 
 int A = angka1 / angka2;
 int B = angka1 % angka2;
